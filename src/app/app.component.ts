025bd154f75a2ba62a7b0d95e42a8aa64435e00a
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-04-24 21:37:27 -0300

    Creacion de componentes y menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { InicioComponent } from "./components/inicio.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InicioComponent, FooterComponent],
+  imports: [RouterOutlet, InicioComponent, FooterComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio.component';
 import { LoginComponent } from './components/login/login.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { CarpetasComponent } from './components/carpetas/carpetas.component';
+import { CalendarioComponent } from './components/calendario/calendario.component';
 
 export const routes: Routes = [
     {
@@ -13,14 +17,26 @@ export const routes: Routes = [
 
     {
         path: 'inicio',
-        component : InicioComponent
-        
-    },
-    {
-      path: 'login',
-      component: LoginComponent
-    }
-
-
-
-];
+        component: InicioComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'perfil',
+        component: PerfilComponent  // Ruta para perfil
+      },
+      {
+        path: 'chat',
+        component: ChatComponent  // Ruta para chat
+      },
+      {
+        path: 'carpeta',
+        component: CarpetasComponent  // Ruta para carpeta
+      },
+      {
+        path: 'calendario',
+        component: CalendarioComponent  // Ruta para calendario
+      }
+    ];
